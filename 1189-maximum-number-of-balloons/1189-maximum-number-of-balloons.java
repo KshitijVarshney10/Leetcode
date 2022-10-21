76fcf856fc9c7c5d667b320cfc25341bf90682d1
Kshitij Varshney <kshitij.varshney1007@gmail.com>
2022-10-21 18:25:30 +0530

    Time: 6 ms (64.76%), Space: 40.5 MB (97.22%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,19 +1,27 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        HashMap<Character, Integer> map=new HashMap<>();
-        map.put('b',0);map.put('a',0);
-        map.put('l',0);map.put('o',0);
-        map.put('n',0);
-        
+//         HashMap<Character, Integer> map=new HashMap<>();
+//         map.put('b',0);map.put('a',0);
+//         map.put('l',0);map.put('o',0);
+//         map.put('n',0);  
+        int b=0,a=0,l=0,o=0,n=0;
         for(int i=0;i<text.length();i++){
-            if(map.containsKey(text.charAt(i)))
-                map.put(text.charAt(i),map.get(text.charAt(i))+1);
+            if(text.charAt(i)=='b') 
+                b+=1;
+            if(text.charAt(i)=='a') 
+                a+=1;
+            if(text.charAt(i)=='l') 
+                l+=1;
+            if(text.charAt(i)=='o') 
+                o+=1;
+            if(text.charAt(i)=='n') 
+                n+=1;
         }
-        int b=map.get('b');
-        int a=map.get('a');
-        int o=map.get('o');
-        int l=map.get('l');
-        int n=map.get('n');
+//         int b=map.get('b');
+//         int a=map.get('a');
+//         int o=map.get('o');
+//         int l=map.get('l');
+//         int n=map.get('n');
         
         return Math.min(Math.min(o/2, l/2), Math.min(Math.min(b, a), n));
     }
