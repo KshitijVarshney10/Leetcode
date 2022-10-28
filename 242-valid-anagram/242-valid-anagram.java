b5a4810fad804218d2cf8c74cfa4815ace048a83
Kshitij Varshney <kshitij.varshney1007@gmail.com>
2022-10-28 21:31:59 +0530

    Time: 2 ms (99.22%), Space: 42.5 MB (89.29%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] cs=s.toCharArray();
+        char[] ct=t.toCharArray();
+        Arrays.sort(cs); Arrays.sort(ct);
+        return String.valueOf(cs).equals(String.valueOf(ct))?true:false;
+    }
+}
