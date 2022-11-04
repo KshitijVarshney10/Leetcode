7180b7e16d17f2cd49274d8452ecc4fdf9f14de6
Kshitij Varshney <kshitij.varshney1007@gmail.com>
2022-11-05 01:29:15 +0530

    Time: 1901 ms (5.02%), Space: 620.5 MB (5.02%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.java
@@ -1,42 +1,26 @@
 class Solution {
     public String reverseVowels(String s) {
-        String ans="";
-        char[] str=s.toCharArray();
-        int i=0,j=s.length()-1;
-        while(i<=j){
-            char beg=str[i];
-            char end=str[j];
-            
+        String vowels="";String ans="";
+        char[] ch=s.toCharArray();
+        for(int i=0;i<s.length();i++){
+            char beg=s.charAt(i);
+            if(beg=='a'||beg=='A'||beg=='e'||beg=='E'||beg=='i'||beg=='I'||beg=='o'||beg=='O'||beg=='u'||beg=='U')
+                vowels+=beg;
+        }
+        StringBuilder sb=new StringBuilder();
+        sb.append(vowels);
+        sb.reverse();
+        vowels=sb.toString();
+        for(int i=0,j=0;i<ch.length;i++){
+            char beg=ch[i];
             if(beg=='a'||beg=='A'||beg=='e'||beg=='E'||beg=='i'||beg=='I'||beg=='o'||beg=='O'||beg=='u'||beg=='U'){
-                if(end=='a'||end=='A'||end=='e'||end=='E'||end=='i'||end=='I'||end=='o'||end=='O'||end=='u'||end=='U'){
-                    char t=beg;
-                    str[i]=end;
-                    str[j]=t;
-                    i++;j--;
-                }
-                else{
-                    j--;
-                }
-            }
-            else if(end=='a'||end=='A'||end=='e'||end=='E'||end=='i'||end=='I'||end=='o'||end=='O'||end=='u'||end=='U'){
-                if(beg=='a'||beg=='A'||beg=='e'||beg=='E'||beg=='i'||beg=='I'||beg=='o'||beg=='O'||beg=='u'||beg=='U'){
-                    char t=beg;
-                    str[i]=end;
-                    str[j]=t;
-                    i++;j--;
-                }
-                else{
-                    i++;
-                }       
+                ans+=vowels.charAt(j);
+                j++;
             }
             else{
-                i++;
-                j--;     
+                ans+=ch[i];
             }
-           
-        }
-        for(int k=0;k<str.length;k++){
-            ans+=str[k];
+                
         }
         return ans;
     }
