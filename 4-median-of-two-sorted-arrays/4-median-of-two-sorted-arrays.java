778792169a0bae4db1dd66da9c0bdf27c02e7cdc
Kshitij Varshney <kshitij.varshney1007@gmail.com>
2022-11-10 15:47:29 +0530

    Time: 4 ms (82.38%), Space: 43.4 MB (86.76%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,5 +1,44 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // if(nums1.length==0)
+        // {
+        //     if(nums2.length%2!=0)
+        //     return (double)nums2[nums2.length/2];
+        //     else
+        //     return (double)(nums2[nums2.length/2]+nums2[nums2.length/2-1])/2;
+        // }
+        // if(nums2.length==0){
+        //     if(nums1.length%2!=0)
+        //     return (double)nums1[nums1.length/2];
+        //     else
+        //     return (double)(nums1[nums1.length/2]+nums1[nums1.length/2-1])/2;
+        // }
+        // int [] f=new int[nums1.length+nums2.length];
+        // int i=0,j=0,k=0;
+        // while(i<nums1.length  && j<nums1.length){
+        //     if(nums1[i]<=nums2[j]){
+        //         f[k]=nums1[i];
+        //         k++;
+        //         i++;
+        //     }
+        //     else{
+        //         f[k]=nums2[j];
+        //         j++;
+        //         i++;
+        //     }
+        // }
+        // while(i<nums1.length)
+        // {
+        //     f[k]=nums1[i];
+        //     k++;
+        //     i++;
+        // }
+        // while(j<nums2.length)
+        // {
+        //     f[k]=nums2[j];
+        //     k++;
+        //     j++;
+        // }
         int [] f=new int[nums1.length+nums2.length];
         int i=0;
         for(int j=0;j<nums1.length;j++,i++){
